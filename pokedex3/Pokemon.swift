4c6983865ca986712b8099a20ab13ec3d52ea45f
barra14 <andresbarrios@Andress-MacBook-Pro.local>
2016-08-21 20:11:23 -0400

    add pod files

--- pokedex3/Pokemon.swift
+++ pokedex3/Pokemon.swift
@@ -7,3 +7,114 @@
 //
 
 import Foundation
+import Alamofire
+
+class Pokemon {
+    
+    private var _name : String!
+    private var _pokedexID : Int!
+    private var _description: String!
+    private var _type: String!
+    private var _defense: String!
+    private var _height: String!
+    private var _weight: String!
+    private var _attack: String!
+    private var _nextEvolutionTxt: String!
+    private var _pokemonURL: String!
+    
+    var nextEvolutionTxt: String{
+        if _nextEvolutionTxt == nil {
+            _nextEvolutionTxt = ""
+        }
+        
+        return _nextEvolutionTxt
+    }
+    
+    var attack : String{
+        if _attack == nil{
+            _attack = ""
+    }
+        return _attack
+    }
+    
+    var weight : String {
+        if _weight == nil{
+            _weight = ""
+    }
+        return _weight
+    }
+    
+    var height : String{
+        if _height == nil{
+            _height = ""
+    }
+        return _height
+    }
+    
+    var defense : String {
+        if _defense == nil{
+            _defense = ""
+    }
+        return _defense
+    }
+    
+    var type: String{
+        if _type == nil{
+            _type = ""
+    }
+        return _type
+    }
+    
+    var description : String{
+        if _description == nil{
+            _description = ""
+    }
+        return _description
+    }
+    
+    var name : String{
+        return _name
+    }
+    
+    var pokedexID: Int {
+        return _pokedexID
+    }
+    
+    init(name : String , pokedexID : Int){
+        self._name = name
+        self._pokedexID = pokedexID
+        
+        self._pokemonURL = "\(URL_BASE)\(URL_POKEMON)\(self._pokedexID)/"
+        
+    }
+    
+    func downloadPokemonDetail( completed: DownloadComplete){
+        
+        Alamofire.request( _pokemonURL, withMethod: .get).responseJSON { (response) in
+            
+            if let dict = response.result.value as? Dictionary<String, AnyObject>{
+            
+            if let weight = dict["weight"] as? String{
+                self._weight = weight
+            }
+            
+            if let height = dict["height"] as? String{
+                self._height = height
+                
+            }
+            
+            if let attack = dict["attack"] as? Int{
+                self._attack =  "\(attack)"
+            }
+            
+            if let defense = dict["defense"] as? Int{
+                self._defense = "\(defense)"
+                }
+                
+                
+            }
+            
+            completed()
+        }
+    }
+}
--- pokedex3/PokemonDetailVC.swift
+++ pokedex3/PokemonDetailVC.swift
@@ -9,27 +9,48 @@
 import UIKit
 
 class PokemonDetailVC: UIViewController {
-
+    
+    var pokemon : Pokemon!
+    
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImgLbl: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexIDLbl: NSLayoutConstraint!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var nextEvolutionLbl: UILabel!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        nameLbl.text = pokemon.name
+        pokemon.downloadPokemonDetail {
+            
+            self.updateUI()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func updateUI(){
+        
+        attackLbl.text = pokemon.attack
+        defenseLbl.text = pokemon.defense
+        heightLbl.text = pokemon.height
+        weightLbl.text = pokemon.weight
+        
     }
-    */
+        
+
+    @IBAction func backBtnPressed(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+    }
 
 }
